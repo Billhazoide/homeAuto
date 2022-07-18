58311c6af2cda284a95663d6fa099a2bd62eb9c3
Gabriel Pereira <gabrielpereira.gps@gmail.com>
2022-07-18 17:23:16 -0300

    .

--- ethernet_module.cpp
+++ ethernet_module.cpp
@@ -3,7 +3,10 @@
 #include <SPI.h>
 
 //Set IP for the main automation server
-byte myIp[]  = { 192, 168, 0, 210 };   
+byte myIp[]  = { 192, 168, 0, 10 };   
+
+// Set Mac address to ethernet shield
+byte myMac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
 
 //Set rx port to OSC
 int  serverPort  = 8000;
@@ -12,24 +15,10 @@ int  serverPort  = 8000;
 int destPort = 9000;
 
 //Set arduino digital port
-int relePin2 =  2;
-int relePin3 =  3;
-int relePin4 =  4;
-int relePin5 =  5;
-int relePin6 =  6;
-int relePin7 =  7;
-int relePin8 =  8;   
-int relePin9 =  9;
+int DigitalPinCount = 9;
 
-// Initial state of the port
-int flag2 = 0;
-int flag3 = 0;
-int flag4 = 0;
-int flag5 = 0;
-int flag6 = 0;
-int flag7 = 0;
-int flag8 = 0;
-int flag9 = 0;
+// Initial state of the port, 1 = Off
+int flag = 1;
 
 //Start OSC server (tread with rx OSC msg)
 OSCServer server;    
@@ -40,74 +29,63 @@ OSCClient client;
 void setup() {
 
   //Start ethernet module interface
-  Ethernet.begin(myIp);  
+  Ethernet.begin(myMac, myIp);  
 
   //Start OSC server setting rx port
   server.begin(serverPort);   
 
-  //set callback function & oscaddress
-  server.addCallback("/ard/relePin2", &func);  
-  server.addCallback("/ard/relePin3", &func2); 
-  server.addCallback("/ard/relePin4", &func3); 
-  server.addCallback("/ard/relePin5", &func4); 
-  server.addCallback("/ard/relePin6", &func5); 
-  server.addCallback("/ard/relePin7", &func6); 
-  server.addCallback("/ard/relePin8", &func7); 
-  server.addCallback("/ard/relePin9", &func8); 
+  //Set callback function to oscaddress (routes)
+  server.addCallback("/ard/relePin2", &func); 
 
-  //Set digital port X as output
-  pinMode(relePin2, OUTPUT);   
-  pinMode(relePin3, OUTPUT);
-  pinMode(relePin4, OUTPUT);
-  pinMode(relePin5, OUTPUT);
-  pinMode(relePin6, OUTPUT);
-  pinMode(relePin7, OUTPUT);
-  pinMode(relePin8, OUTPUT);
-  pinMode(relePin9, OUTPUT);
-
+  // Call function
+  setupPins();
 }
   
 void loop(){
-  
-  //Check for new OSC msg
-  if(server.availableCheck()>0){   
-    Serial.println("alive, mf!"); 
+  // Check for any new OSC msg
+  if(server.availableCheck()>0) {
+    // Serial.println("alive, mf! "); 
   }
-  
+}
+
+void setupPins(){
+  //Set digital port X as output
+  for(int i=0; i < DigitalPinCount; i++){
+    pinMode(i, OUTPUT);
+  }
 }
 
 // Tread OSC message
-void func(OSCMessage *_mes) { 
-  // Store value the args from OSC ("/ard/relePin2")
-  int value = (int)_mes->getArgFloat(0);
+void func(OSCMessage * _mes) { 
+  // Store value the args from OSC client
+  int value = (int)_mes -> getArgFloat(0);
 
-  // Switch digital port
-  digitalWrite(relePin2, value);
-
+  // Switch digital port value
+  digitalWrite(2, value);
+  
   // Create new osc message
   OSCMessage txMes;
 
   // Set destination ip address & destination OSC port
   txMes.setAddress(_mes->getIpAddress(), destPort);
   
-  // Set OSC command (route)
+  // Set OSC command
   txMes.beginMessage("/ard/relePin2");
   
   // Set port state in OSC message 
-  txMes.addArgFloat(flag2);
-
-  if (flag2 == 1) {
-    flag2 = 0;
-    digitalWrite(relePin2, LOW);
+  txMes.addArgFloat(flag);
+  
+  if (flag == 1) {
+    flag = 0;
+    digitalWrite(2, LOW);
   } else {
-    flag2 = 1;
-    digitalWrite(relePin2, HIGH);
+    flag = 1;
+    digitalWrite(2, HIGH);
   }
-
+  
   // Set port status in OSC message
-  txMes.addArgInt32(flag2);
+  txMes.addArgInt32(flag);
 
   // Send OSC message to client 
   client.send(&txMes);
-
 }
