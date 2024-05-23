bd0dc154d518c2e73a3bc3c1fc1d8b2ea749948b
габриел перейра <53276690+Billhazoide@users.noreply.github.com>
2024-05-23 17:11:03 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,101 +3,101 @@
 #include <SPI.h>
 #include <IRremote.hpp>
 
-// Network Configuration
-const byte MY_IP[] = {192, 168, 1, 177};
-const byte MY_MAC[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
-const int SERVER_PORT = 8000;
-const int DEST_PORT = 9000;
+// Ethernet configuration
+const byte myIp[] = {192, 168, 1, 177};
+const byte myMac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
+const int serverPort = 8000;
+const int destPort = 9000;
 
-// Pins
-const int DIGITAL_PIN_COUNT = 11;
+// IR setup
+IRsend irsend;
 
-// IR Sender
-IRsend irSender;
+// Pin configuration
+const int DigitalPinCount = 11;
+int flags[DigitalPinCount] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
 
-// Flags for digital pins
-int flags[DIGITAL_PIN_COUNT] = {1};
-
-// OSC Server and Client
+// OSC server and client
 OSCServer server;
 OSCClient client;
 
-// Function prototypes
-void setupPins();
-void handleRelayOSC(OSCMessage *_mes, int pin);
-void handlePWMOSC(OSCMessage *_mes, int pin);
-void handleTVControlOSC(OSCMessage *_mes, int command);
-
 void setup() {
   Serial.begin(9600);
+  
+  // Ethernet setup
+  Ethernet.begin(myMac, myIp);
+  server.begin(serverPort);
 
-  // Initialize IR Sender
-  irSender.begin(4, ENABLE_LED_FEEDBACK);
-
-  // Start Ethernet
-  Ethernet.begin(MY_MAC, MY_IP);
-
-  // Start OSC Server
-  server.begin(SERVER_PORT);
-
-  // Set up pins
+  // OSC callbacks
+  setupOSC();
+  
+  // Initialize pins
   setupPins();
 }
 
 void loop() {
-  // Check for OSC messages
-  if (server.availableCheck() > 0) {
-    // Handle OSC messages
-  }
+  // server.parse();
+  server.availableCheck();
+}
 
-  // Handle other tasks or sensors
+void setupOSC() {
+  server.addCallback("/ard/relePin2", &togglePin);
+  server.addCallback("/ard/relePin3", &togglePin);
+  server.addCallback("/ard/pwmPin3", &setPwm);
+  // server.addCallback("/ard/pwmPin4", &handleTVControlOSC);
+  // Add more callbacks as needed
 }
 
 void setupPins() {
-  for (int i = 2; i < DIGITAL_PIN_COUNT; i++) {
+  for (int i = 1; i < DigitalPinCount; i++) {
     pinMode(i, OUTPUT);
   }
 }
 
-void handleRelayOSC(OSCMessage *_mes, int pin) {
-  int value = (int)_mes->getArgFloat(0);
-  digitalWrite(pin, value);
-  flags[pin] = value;
+void togglePin(OSCMessage *_mes) {
+  // int pin = _mes->getArgInt32(0);
+  String address = _mes->getOSCAddress();
+  int pin = address.substring(12).toInt();
 
-  // Create response OSC message
-  OSCMessage txMes;
-  txMes.setAddress(_mes->getIpAddress(), DEST_PORT);
-  txMes.beginMessage(("/ard/relePin" + String(pin)).c_str());
-  txMes.addArgInt32(flags[pin]);
-  client.send(&txMes);
+  flags[pin] = !flags[pin];
+
+  digitalWrite(pin, flags[pin]);
+
+  sendOSCMessage("/ard/relePin" + String(pin), flags[pin], _mes );
 }
 
-void handlePWMOSC(OSCMessage *_mes, int pin) {
-  int value = (int)_mes->getArgFloat(0);
-  analogWrite(pin, value);
+void setPwm(OSCMessage *_mes) {
 
-  // Create response OSC message
+  String address = _mes->getOSCAddress();
+  int pin = address.substring(11).toInt();
+
+  // int pin = _mes->getArgInt32(0);
+  int value = _mes->getArgInt32(1);
+  analogWrite(pin, value);
+  sendOSCMessage("/ard/pwmPin" + String(pin), value, _mes);
+}
+
+// void handleTVControlOSC(OSCMessage *_mes, int command) {
+//   // Send IR signal based on command
+//   // Those IR codes are to controll Phillips tv
+//   switch (command) {
+//     case 11: irSender.sendRC6(0x0, 0x10058, 0, true); break;  // Arrow up
+//     case 12: irSender.sendRC6(0x0, 0x1005A, 0, true); break;  // Arrow left
+//     case 13: irSender.sendRC6(0x0, 0x10059, 0, true); break;  // Arrow down
+//     case 14: irSender.sendRC6(0x0, 0x1005B, 0, true); break;  // Arrow right
+//     case 15: irSender.sendRC6(0x0, 0x1005C, 0, true); break;  // OK button
+//     case 16: irSender.sendRC6(0x0, 0x10010, 0, true); break; // Volume up
+//     case 17: irSender.sendRC6(0x0, 0x10011, 0, true); break; // Volume down
+//     case 18: irSender.sendRC6(0x0, 0x1000D, 0, true); break; // Mute
+//     case 19: irSender.sendRC6(0x0, 0x10040, 0, true); break; // Options
+//     case 20: irSender.sendRC6(0x0, 0x10054, 0, true); break; // Home
+//     case 21: irSender.sendRC6(0x0, 0x1000C, 0, true); break; // On/Off
+//     default: break;
+//   }
+
+void sendOSCMessage(String address, int value, OSCMessage * _mes) {
   OSCMessage txMes;
-  txMes.setAddress(_mes->getIpAddress(), DEST_PORT);
-  txMes.beginMessage(("/ard/pwmPin" + String(pin)).c_str());
+  txMes.setAddress(_mes->getIpAddress(), destPort);
+  txMes.beginMessage(address.c_str());
   txMes.addArgInt32(value);
   client.send(&txMes);
-}
-
-void handleTVControlOSC(OSCMessage *_mes, int command) {
-  // Send IR signal based on command
-  switch (command) {
-    case 11: irSender.sendRC6(0x0, 0x10058, 0, true); break;  // Arrow up
-    case 12: irSender.sendRC6(0x0, 0x1005A, 0, true); break;  // Arrow left
-    case 13: irSender.sendRC6(0x0, 0x10059, 0, true); break;  // Arrow down
-    case 14: irSender.sendRC6(0x0, 0x1005B, 0, true); break;  // Arrow right
-    case 15: irSender.sendRC6(0x0, 0x1005C, 0, true); break;  // OK button
-    case 16: irSender.sendRC6(0x0, 0x10010, 0, true); break; // Volume up
-    case 17: irSender.sendRC6(0x0, 0x10011, 0, true); break; // Volume down
-    case 18: irSender.sendRC6(0x0, 0x1000D, 0, true); break; // Mute
-    case 19: irSender.sendRC6(0x0, 0x10040, 0, true); break; // Options
-    case 20: irSender.sendRC6(0x0, 0x10054, 0, true); break; // Home
-    case 21: irSender.sendRC6(0x0, 0x1000C, 0, true); break; // On/Off
-    default: break;
-  }
 }
